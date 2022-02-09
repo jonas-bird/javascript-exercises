56abd0cc5d7c0c7f8915f15faec7466a23ceb16d
Jonas Bird <200~91971493+jonas-bird@users.noreply.github.com~>
2022-02-08 20:21:10 -0500

    Complete first version of exercise 03
    Modify reverseString.js so that it passes tests
    
    TODO Try out a more interesting way to solve this problem

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(aString) {
+// Function takes a string and returns its reverse
+  let revString = "";
+  for(let i = aString.length - 1; i >= 0; i--) {
+    revString += aString[i];
+  }
+  return revString
 };
 
 // Do not edit below this line
