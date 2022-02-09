2dc61667636c00feef47c7060ff6f9c38353a8d2
Jonas Bird <200~91971493+jonas-bird@users.noreply.github.com~>
2022-02-08 20:09:32 -0500

    Completed exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(word, number) {
+  /*function that takes a string and a number and returns the string repeated the number of times
+   * */
+  let accumulator = "";
+  if (number < 0){
+    return ("ERROR")
+  }
+  for (let i = 0; i < number; i++) {
+    accumulator += word;
+  }
+  return accumulator;
 };
 
 // Do not edit below this line
